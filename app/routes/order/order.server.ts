a04e52b354c58fe0155342051611c8cc076776b1
Julian Jark <juliuxu@gmail.com>
2023-04-28 17:09:47 +0200

    hmm

--- app/routes/order/order.server.ts
+++ app/routes/order/order.server.ts
@@ -205,9 +205,6 @@ export async function placeOrder(
     await page.waitForLoadState();
     log({ data: "got load state" });
 
-    await page.waitForLoadState();
-    log({ data: "got another" });
-
     const reservantionLine = await page
       .locator(".rental-id")
       .first()
