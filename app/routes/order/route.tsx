440b2e7c2547d5326271a9d710ac5bf2da32ba45
Julian Jark <juliuxu@gmail.com>
2023-05-04 22:46:18 +0200

    add some info without any styling

--- app/routes/order/route.tsx
+++ app/routes/order/route.tsx
@@ -1,10 +1,17 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-import { useSearchParams } from "@remix-run/react";
+import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 import { useState } from "react";
+import { getOrderInfo } from "../api.order/schema";
 
 const title = "🧖 Bestill Badstue 🌊";
 export const meta: V2_MetaFunction = () => {
   return [{ title }];
+};
+
+export const loader = ({ request }: LoaderArgs) => {
+  const orderInfo = getOrderInfo(request.url);
+  return json({ orderInfo });
 };
 
 type EventType = {
@@ -36,9 +43,38 @@ export default function Component() {
     };
   };
 
+  const { orderInfo } = useLoaderData<typeof loader>();
+
+  // Formatting
+  const formatter = new Intl.DateTimeFormat("no-nb", { dateStyle: "full" });
+
+  // 07 -> 07:00
+  // 7 -> 07:00
+  // 8.5 -> 08:30
+  const shortTimeToClockTime = (timeString: string) => {
+    const leftPart = timeString.split(".")[0].padStart(2, "0");
+    let rightPart = timeString.split(".")[1] ?? "00";
+    rightPart = rightPart.replace("5", "30");
+    return `${leftPart}:${rightPart}`;
+  };
+
   return (
     <main className="container">
       <h1>{title}</h1>
+      {/* INFO */}
+      <p>
+        <strong>
+          {orderInfo.fornavn} {orderInfo.etternavn}
+        </strong>
+        {" bestill "}
+        {orderInfo.antall} {orderInfo.antall === 1 ? "billett" : "billetter"}
+        {" på "}
+        <strong>{orderInfo.sted}</strong>
+        {" for "}
+        {" klokken "}
+        <time>{shortTimeToClockTime(orderInfo.time)}</time>{" "}
+        <time>{formatter.format(new Date(orderInfo.date))}</time>
+      </p>
       <div>
         {!isOrdering && (
           <button
